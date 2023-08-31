008eebf0f5638a29e82f781863c3a0d73cd9d2ae
akmalhazriq <62550064+PishangShedappp@users.noreply.github.com>
2023-08-31 18:28:53 +0800

    main update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import { ThemeProvider } from 'next-themes'
 
 const inter = Inter({ subsets: ['latin'] })
 
+document.title = "Lisker"
+
 export default function RootLayout({children,}:{children: React.ReactNode}) {
   return (
     <html lang="en">
