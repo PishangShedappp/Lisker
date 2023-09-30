20505b26cb9891d715b9d3bf23ff3277eba5b6a1
akmalhazriq <62550064+PishangShedappp@users.noreply.github.com>
2023-09-30 22:05:20 +0800

    features / added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@
 
 import react, {useEffect} from 'react';
 import {AiOutlineMenu} from 'react-icons/ai';
+import {MdOutlineAttachMoney} from 'react-icons/md';
+import {FaUserGraduate} from 'react-icons/fa6';
+import {FaFeatherAlt} from 'react-icons/fa';
+import {BsFillShieldFill} from 'react-icons/bs';
 import Link from 'next/link';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 import './page.css'
-import rightHero from '../../public/undraw_Make_it_rain_re_w9pc.png';
 
 export default function Page() {
   useEffect(() => {
@@ -27,7 +30,7 @@ export default function Page() {
           <nav className='navbar'>
             <ul>
               <li>
-                <Link href='/auth/register' className='login'>Log In</Link>
+                <Link href='/auth/login' className='login'>Log In</Link>
               </li>
             </ul>
             <AiOutlineMenu className='mobile-nav-toggle'/>
@@ -53,6 +56,45 @@ export default function Page() {
         </div>
       </div>
       {/* HERO END */}
+      {/* FEATURES START */}
+      <div className="features">
+        <div className="features_container">
+          <div className='line_features' data-aos='zoom-out'></div>
+          <div className="features_row">
+            <div className="features_item_container" data-aos='zoom-out'>
+              <div className="features_item">
+                <MdOutlineAttachMoney className='features_icon'/>
+                <h4>Affordable</h4>
+                <p>It's suitable for anyone who want to save their money and limit some budget.</p>
+              </div>
+            </div>
+            <div className="features_item_container" data-aos='zoom-out'>
+              <div className="features_item">
+                <FaUserGraduate className='features_icon'/>
+                <h4>User Friendly</h4>
+                <p>The UI is user friendly that means it's easy to learn for any age either young or old.</p>
+              </div>
+            </div>
+            <div className="features_item_container" data-aos='zoom-out'>
+              <div className="features_item">
+                <BsFillShieldFill className='features_icon'/>
+                <h4>Security</h4>
+                <p>Secure your personal or group project notes from any other unauthorized person.</p>
+              </div>
+            </div>
+            <div className="features_item_container" data-aos='zoom-out'>
+              <div className="features_item">
+                <FaFeatherAlt className='features_icon'/>
+                <h4>Lightweight</h4>
+                <p>This tools can be used by any other platform like web, mobile and desktop application.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* FEATURES END */}
+      {/* PRICING START */}
+      {/* PRICING END */}
     </div>
   )
 }
